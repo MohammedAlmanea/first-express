96a1814dbc746af96d886c6ac536f9d85b1162ab
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-09 11:35:14 +0300

    build the new routes

--- build/index.js
+++ build/index.js
@@ -4,15 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const index_1 = __importDefault(require("./routes/index"));
 const app = (0, express_1.default)();
 const port = 3000;
 //set Endpoint
-app.get('/api', (req, res) => {
-    res.send('This is the /api route ');
-});
-app.get('', (req, res) => {
-    res.send('server working!');
-});
+app.use('/api', index_1.default);
 //check for port to avoid already in use error testing
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
