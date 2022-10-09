26f3089c5b2fa789bb72f77ce93c7435e3cea04d
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-09 11:27:24 +0300

    Completed teachers route and changed api main route message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import teachers from './api/teachers';
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  res.send('connected');
+  res.send('connected to main route');
 });
 
+routes.use('/teachers',teachers);
 export default routes;
